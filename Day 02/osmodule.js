bb49ed7489db3ac7a0e16fa1af4d15d15d21d090
Vivek Mane <vivekmane172@gmail.com>
2026-07-15 10:12:20 +0530

    OS module

---
+++ Day 02/osmodule.js
@@ -0,0 +1,17 @@
+const os = require('os')
+
+// Returns the free memory in the system in bytes as an integer
+console.log(os.freemem());
+
+// Returns the home directory of the current user
+console.log(os.homedir());
+
+//Returns the OS platform
+console.log(os.platform());
+
+//Returns the OS version
+console.log(os.version());
+
+// Returns the type of the OS
+console.log(os.type());
+
