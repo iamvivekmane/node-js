e566e9547d62c89a9c477ac848ed6630ac1fa521
Vivek Mane <vivekmane172@gmail.com>
2026-07-20 21:01:10 +0530

    Served an index.html file using server

---
+++ Day 04/server.js
@@ -0,0 +1,19 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+const server = http.createServer((req, res) => {
+    fs.readFile(path.join(__dirname, 'index.html'), (err, data) => {
+        if (err) {
+            res.writeHead(500);
+            res.end('Error loading index.html');
+            return;
+        }
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.end(data);
+    });
+});
+
+server.listen(3000, () => {
+    console.log('Server running at http://localhost:3000');
+});
